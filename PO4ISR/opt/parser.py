6d19e4b273e14d1354db9be4a764f5d6808d72cb
Nhatto20 <121470232+Nhatto20@users.noreply.github.com>
2024-10-29 15:05:58 +0700

    Add files via upload

---
+++ PO4ISR/opt/parser.py
@@ -0,0 +1,44 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='APO4Rec')
+    parser.add_argument('--reward_func', 
+                        type=str,
+                        default='ndcg',
+                        help='how the reward is calculated, options: ndcg')
+    parser.add_argument('--model', 
+                        type=str,
+                        default='llama3-8b-8192',
+                        help='options: llama3-8b-8192, llama3-70b-8192') 
+    # parser.add_argument('--model', 
+    #                     type=str,
+    #                     default='gpt-3.5-turbo',
+    #                     help='which model as recommender, options: gpt-3.5-turbo') 
+    parser.add_argument('--seed', 
+                        type=int,
+                        default=42,
+                        help='options: 42, 625, 2023, 0, 10')
+    parser.add_argument('--data_path', 
+                        type=str,
+                        default='C:/Users/japan/OneDrive/Documents/GitHub/PO4ISR/Dataset/',
+                        help='help yourself')
+    parser.add_argument('--candidate_size', 
+                        type=int,
+                        default=20,
+                        help='options: 10, 20')
+    parser.add_argument('--dataset', 
+                        type=str,
+                        default='bundle',
+                        help='use which datset: bundle/games/ml-1m')
+    parser.add_argument('--train_num', 
+                        type=int,
+                        default=50,
+                        help='options: 50,150')
+    parser.add_argument('--batch_size', 
+                        type=int,
+                        default=16,
+                        help='options: 16,32')
+
+    args = parser.parse_args()
+    
+    return args
---
+++ PO4ISR/opt/request.py
@@ -0,0 +1,73 @@
+import openai
+import random
+import time
+
+from groq import Groq
+
+class Request():
+    def __init__(self, config):
+        self.conifg = config
+        #openai.api_key = self.conifg['openai_api_key']
+        self.holder = Groq(api_key = self.conifg['openai_api_key'])
+    
+    def request(self, user, system=None, message=None):
+        response = self.openai_request(user, system, message)
+
+        return response
+    
+    def openai_request(self, user, system=None, message=None):
+
+        if system:
+            message=[{"role":"system", "content":system}, {"role": "user", "content": user}]
+        else:
+            content = system + user
+            message=[{"role": "user", "content": content}]
+        model = self.conifg['model']
+        for delay_secs in (2**x for x in range(0, 10)):
+            try:
+                response = self.holder.chat.completions.create(
+                    model=model,
+                    messages = message,
+                    temperature=0.2
+                    #frequency_penalty=0.0
+                )
+                break
+            except Exception as e:
+                randomness_collision_avoidance = random.randint(0, 1000) / 1000.0
+                sleep_dur = delay_secs + randomness_collision_avoidance
+                print(f"Error: {e}. Retrying in {round(sleep_dur, 2)} seconds.")
+                time.sleep(sleep_dur)
+                continue
+        
+        return response.choices[0].message.content
+
+
+
+
+    # def openai_request(self, user, system=None, message=None):
+    #     '''
+    #     fix openai communicating error
+    #     https://community.openai.com/t/openai-error-serviceunavailableerror-the-server-is-overloaded-or-not-ready-yet/32670/19
+    #     '''
+    #     if system:
+    #         message=[{"role":"system", "content":system}, {"role": "user", "content": user}]
+    #     else:
+    #         content = system + user
+    #         message=[{"role": "user", "content": content}]
+    #     model = self.conifg['model']
+    #     for delay_secs in (2**x for x in range(0, 10)):
+    #         try:
+    #             response = openai.ChatCompletion.create(
+    #                 model=model,
+    #                 messages = message,
+    #                 temperature=0.2,
+    #                 frequency_penalty=0.0)
+    #             break
+    #         except openai.OpenAIError as e:
+    #             randomness_collision_avoidance = random.randint(0, 1000) / 1000.0
+    #             sleep_dur = delay_secs + randomness_collision_avoidance
+    #             print(f"Error: {e}. Retrying in {round(sleep_dur, 2)} seconds.")
+    #             time.sleep(sleep_dur)
+    #             continue
+        
+    #     return response["choices"][0]["message"]["content"]
